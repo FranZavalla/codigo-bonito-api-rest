c452615a48065d7fc2f23b3585832cb7828edf4a
Francisco Zavalla <franzavalla85@gmail.com>
2025-05-17 18:30:27 -0300

    Delete dependency_injection directory

--- dependency_injection/terrain/rectangle_terrain.py
+++
@@ -1,9 +0,0 @@
-from dependency_injection.terrain.terrain import Terrain
-
-class RectangleTerrain(Terrain):
-    def __init__(self, length: float, width: float):
-        self.length = length
-        self.width = width
-
-    def get_area(self) -> float:
-        return self.length * self.width
--- dependency_injection/terrain/triangular_terrain.py
+++
@@ -1,9 +0,0 @@
-from dependency_injection.terrain.terrain import Terrain
-
-class TriangularTerrain(Terrain):
-    def __init__(self, base: float, height: float):
-        self.base = base
-        self.height = height
-
-    def get_area(self) -> float:
-        return 0.5 * self.base * self.height
