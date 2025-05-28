62975bcc6a4f39b25cef2f06c01d84790bec61be
FranZavalla <franzavalla85@gmail.com>
2025-05-28 10:20:17 -0300

    feat: added more tests and improved test names

--- testing/unit/test_bluelytics_connector.py
+++ testing/unit/test_bluelytics_connector.py
@@ -13,7 +13,7 @@ from app.layer_1_data_access.connectors.bluelytics_connector import (
 )
 
 
-def test_get_prices():
+def test_get_prices_return_avg_value_on_success():
     mock_response = get_happy_mock_response()
     with patch("requests.get", return_value=mock_response):
         connector = BluelyticsConnector()
@@ -21,8 +21,16 @@ def test_get_prices():
         assert price == 1
 
 
+def test_get_prices_call_once_on_success():
+    mock_response = get_happy_mock_response()
+    with patch("requests.get", return_value=mock_response) as mock_get:
+        connector = BluelyticsConnector()
+        connector.get_price()
+        mock_get.assert_called_once_with(connector.endpoint)
+
+
 @pytest.mark.parametrize("avg_value", [0.0005, 1000, 1e9])
-def test_get_prices_avg_value(avg_value):
+def test_get_prices_success_edge_cases(avg_value):
     mock_response = get_happy_mock_response(avg_value)
     with patch("requests.get", return_value=mock_response):
         connector = BluelyticsConnector()
@@ -30,7 +38,7 @@ def test_get_prices_avg_value(avg_value):
         assert price == avg_value
 
 
-def test_get_prices_bad_status():
+def test_get_prices_raises_http_error_on_bad_status():
     mock_response = get_bad_status_mock_response()
     with patch("requests.get", return_value=mock_response):
         connector = BluelyticsConnector()
@@ -38,7 +46,7 @@ def test_get_prices_bad_status():
             connector.get_price()
 
 
-def test_get_prices_bad_parsing():
+def test_get_prices_raises_value_error_on_bad_parsing():
     mock_response = get_bad_parsing_mock_response()
     with patch("requests.get", return_value=mock_response):
         connector = BluelyticsConnector()
@@ -46,7 +54,7 @@ def test_get_prices_bad_parsing():
             connector.get_price()
 
 
-def test_get_prices_bad_model():
+def test_get_prices_raises_value_error_on_bad_model():
     mock_response = get_bad_model_mock_response()
     with patch("requests.get", return_value=mock_response):
         connector = BluelyticsConnector()
